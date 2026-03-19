15cc561273e780c4582261219471fe866cc4f374
Nina Krüger <nina.kru@gmx.de>
2026-03-19 15:15:32 +0000

    model evaluation script added

---
+++ src/models/evaluation.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+import numpy as np
+from joblib import load
+import json
+from pathlib import Path
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+X_test = pd.read_csv('data/processed/X_test_scaled.csv')
+y_test = pd.read_csv('data/processed/y_test.csv')
+y_test = np.ravel(y_test)
+
+def main(repo_path):
+    model = load(repo_path / "models/trained_model.joblib")
+    predictions = model.predict(X_test)
+    mse = mean_squared_error(y_test, predictions)
+    mae = mean_absolute_error(y_test, predictions)
+    r2 = r2_score(y_test, predictions)
+    metrics = {"MSE": mse, "MAE": mae, "R2": r2}
+    metric_path = repo_path / "metrics/scores.json"
+    
+    with open(metric_path, "w") as f:
+        json.dump(metrics, f)
+
+if __name__ == "__main__":
+    repo_path = Path(__file__).parent.parent.parent
+    main(repo_path)
